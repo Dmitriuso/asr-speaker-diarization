bc464530ff76c4d207fe3307a39ee4c283ee83e4
DO <dmitri.osipov@lexistems.com>
2022-02-19 12:15:12 +0300

    create venv

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,5 @@
+python3.8 -m venv venv
+source venv/bin/activate
 apt-get install sox libsndfile1 ffmpeg
 pip install -r requirements.txt
 pip install https://github.com/kpu/kenlm/archive/master.zip
